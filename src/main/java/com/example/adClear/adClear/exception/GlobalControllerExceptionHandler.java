89b14b40407e391fe7eba8bf99e14b85d4fb1864
AngelThread <ytr.ugur@gmail.com>
2018-10-01 20:42:15 +0200

    Adding invalid request hanldler method is called test through controller

--- src/main/java/com/example/adClear/adClear/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/example/adClear/adClear/exception/GlobalControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,7 +28,7 @@ public class GlobalControllerExceptionHandler {
 
 
     @ResponseStatus(HttpStatus.CONFLICT)  // 409
-    @ExceptionHandler({JsonParseException.class, InvalidRequestException.class,MethodArgumentNotValidException.class})
+    @ExceptionHandler({JsonParseException.class, InvalidRequestException.class,MethodArgumentNotValidException.class,HttpMessageNotReadableException.class})
     public ResponseEntity<Object> handleRequestsWithError(HttpServletRequest req, Exception ex) {
 
         String clientId = findClientIdFromRequest(req);
--- src/test/java/com/example/adClear/adClear/controller/CreateClientIntegrationTest.java
+++ src/test/java/com/example/adClear/adClear/controller/CreateClientIntegrationTest.java
@@ -13,8 +13,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,7 +33,7 @@ public class CreateClientIntegrationTest {
     HourStatService hourStatService;
 
     @Test
-    public void clientRequest() throws Exception {
+    public void clientRequestWithNullFields() throws Exception {
         given(hourStatService.handleClientRequest(any())).willReturn(new HourlyStat());
 
         final ObjectMapper objectMapper = new ObjectMapper();
@@ -36,15 +42,56 @@ public class CreateClientIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsBytes(new ClientRequestDto())))
                 .andExpect(status().is(400));
+           verify(hourStatService, times(1)).addInvalidRequestToStats(anyLong(), any(Timestamp.class));
+
     }
 
-//    public static String asJsonString(final Object obj) {
-//        try {
-//            final ObjectMapper mapper = new ObjectMapper();
-//            final String jsonContent = mapper.writeValueAsString(obj);
-//            return jsonContent;
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
+    @Test
+    public void clientRequestWithNullBody() throws Exception {
+        given(hourStatService.handleClientRequest(any())).willReturn(new HourlyStat());
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        mockMvc.perform(post("/clients/2/request")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(null)))
+                .andExpect(status().is(400));
+        verify(hourStatService, times(1)).addInvalidRequestToStats(anyLong(), any(Timestamp.class));
+
+    }
+
+    @Test
+    public void requestWithSuccessReturnCode() throws Exception {
+        given(hourStatService.handleClientRequest(any())).willReturn(new HourlyStat());
+        final ObjectMapper objectMapper = new ObjectMapper();
+        ClientRequestDto clientRequestDto = new ClientRequestDto();
+        clientRequestDto.setTimestamp(Timestamp.valueOf(LocalDateTime.now()).toString());
+        clientRequestDto.setCustomerID(2);
+        clientRequestDto.setUserID("2");
+        clientRequestDto.setTagID(2);
+        clientRequestDto.setRemoteIP("RemoteIp");
+
+
+        mockMvc.perform(post("/clients/2/request")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(clientRequestDto)))
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    public void malformedJson() throws Exception {
+        given(hourStatService.handleClientRequest(any())).willReturn(new HourlyStat());
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+
+        String malformedJson = "{\"\",,}";
+        mockMvc.perform(post("/clients/2/request")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(malformedJson)))
+                .andExpect(status().is(400));
+
+        verify(hourStatService, times(1)).addInvalidRequestToStats(anyLong(), any(Timestamp.class));
+    }
+
+
 }
